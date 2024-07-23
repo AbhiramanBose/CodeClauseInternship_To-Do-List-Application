5c6faacc7557702f737ecebdefda271584d97fe9
ABHIRAMAN BOSE <abhiraman432@gmail.com>
2024-07-23 21:10:50 +0530

    first commit

---
+++ src/main/java/com/mycompany/todolist/ToDoList.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.todolist;
+
+/**
+ *
+ * @author ABHIRAMAN BOSE
+ */
+public class ToDoList {
+
+    public static void main(String[] args) {
+        new AppFrame();
+    }
+}
